678d60aba1e936e5b1a93a4e1f7f97826fe6e3b0
SilentTearsDev <gyurci.dev@gmail.com>
2026-01-30 17:46:18 +0100

    .

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,18 +1,11 @@
-import torch
-from TTS.api import TTS
+import pyttsx3
 
-# Get device
-device = "cuda" if torch.cuda.is_available() else "cpu"
+Input = input("Enter the text you want to convert to speech: ")
 
-# List available 🐸TTS models
-print(TTS().list_models())
+engine = pyttsx3.init()
+engine.setProperty("rate", 150)
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
 
-# Init TTS
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
-
-# Run TTS
-# ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
-# Text to speech list of amplitude values as output
-wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
-# Text to speech to a file
-tts.tts_to_file(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en", file_path="output.wav")
+engine.say(Input)
+engine.runAndWait()
